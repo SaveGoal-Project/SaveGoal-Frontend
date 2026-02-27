25ae4cc8b66391fbefbbe2644de8bbcc28c1f660
Jayden <osafojayden290405@gmail.com>
2026-02-27 21:38:44 +0000

    changes

--- next.config.ts
+++ next.config.ts
@@ -12,9 +12,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
+        hostname: "**",
       },
     ],
   },
